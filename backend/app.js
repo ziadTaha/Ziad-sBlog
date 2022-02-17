cd54dfd1eb1b817f067258b1a56c8df69821321f
ziadTaha <ziadtaha197@gmail.com>
2022-02-17 04:22:14 +0200

    uibuild

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,11 @@ app.use('/api/problems', problemsRouter)
 app.use('/api/comments', commentRouter)
 
 if (process.env.NODE_ENV === 'production') {
+    console.log(path.resolve(__dirname,'../backend/build', 'index.html'))
     app.use(express.static(path.join(__dirname, '/build')))
   
     app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname,'/build', 'index.html'))
+      res.sendFile(path.resolve(__dirname,'../backend/build', 'index.html'))
     )
   } else {
     app.get('/', (req, res) => {
